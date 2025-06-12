b75a39d7ec7ecb598a966d93567d1aa17e4cc258
shubhamsinha <shubham.sinha92@gmail.com>
2025-06-11 17:04:32 -0700

    change money to Numeric type

--- src/tds/codec/column_data/money.rs
+++ src/tds/codec/column_data/money.rs
@@ -1,24 +1,22 @@
-use crate::{error::Error, sql_read_bytes::SqlReadBytes, ColumnData};
+use crate::{error::Error, sql_read_bytes::SqlReadBytes, ColumnData, numeric::Numeric};
 
 pub(crate) async fn decode<R>(src: &mut R, len: u8) -> crate::Result<ColumnData<'static>>
 where
     R: SqlReadBytes + Unpin,
 {
     let res = match len {
-        0 => ColumnData::F64(None),
-        4 => ColumnData::F64(Some(src.read_i32_le().await? as f64 / 1e4)),
-        8 => ColumnData::F64(Some({
+        0 => ColumnData::Numeric(None),
+        4 => {
+            let value = src.read_i32_le().await? as i128;
+            ColumnData::Numeric(Some(Numeric::new_with_scale(value, 4)))
+        },
+        8 => {
             let high = src.read_i32_le().await? as i64;
-            let low = src.read_u32_le().await? as f64;
-
-            ((high << 32) as f64 + low) / 1e4
-        })),
-        _ => {
-            return Err(Error::Protocol(
-                format!("money: length of {} is invalid", len).into(),
-            ))
-        }
+            let low = src.read_u32_le().await? as i64;
+            let value = ((high << 32) + low) as i128;
+            ColumnData::Numeric(Some(Numeric::new_with_scale(value, 4)))
+        },
+        _ => return Err(Error::Protocol(format!("money: length of {} is invalid", len).into()))
     };
-
     Ok(res)
 }
